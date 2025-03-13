93c3d350cc7b038e3e6fef807869123ca6ef40f8
sumi2901 <bahardeen784gmail.com>
2025-03-13 11:22:21 +0530

    fix

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+public class arithmetic{
+    public static void main(String []args)
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        int d=a*b;
+        System.out.println("addition of two numbers:"+c);
+        System.out.println("multiplication of two numbers:"+d);
+    }
+}
