e419b0b2ee45634eb10cbc6048b4f1b928ca3719
sumi2901 <bahardeen784gmail.com>
2025-03-13 14:38:15 +0530

    fix

---
+++ major.java
@@ -0,0 +1,12 @@
+public class major {
+    public static void main (String[]args)
+    {
+        int a=19;
+        if(a>=18){
+        System.out.println("person is major");}
+        else{
+            System.out.println("person is minor");
+        }
+    }
+    
+}
