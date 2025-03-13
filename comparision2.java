0cf19b36c399934c792ed7a5af25b18d082b6738
sumi2901 <bahardeen784gmail.com>
2025-03-13 12:08:48 +0530

    fix

---
+++ comparision2.java
@@ -0,0 +1,15 @@
+public class comparision2{
+    public static void main(String[]args)
+    {
+  int a=10;
+  int b=20;
+  if(a>b)
+  {
+  System.out.println("True");
+  }
+  else
+  {
+  System.out.println("False");
+  }
+}
+}
