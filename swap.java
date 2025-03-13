8959de64108dfd7c4270daf21aada0c0f0da6274
sumi2901 <bahardeen784gmail.com>
2025-03-13 13:44:50 +0530

    fix

---
+++ swap.java
@@ -0,0 +1,10 @@
+public class swap {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        a=(5+6)-5;
+        b=(5+6)-6;
+System.out.println("the value of a is:"+a);
+System.out.println("the value of b is:"+b);
+    }
+}
