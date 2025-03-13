9c5adf94840c9829f2cfc47929545ef51cc3eccd
sumi2901 <bahardeen784gmail.com>
2025-03-13 15:32:52 +0530

    fix

---
+++ v.java
@@ -0,0 +1,14 @@
+public class v {
+    public static void main(String[]args)
+    {
+        int a= 10;
+        if(a==10)
+        System.out.println("a is equal to 10");
+        if(a>10)
+        System.out.println("a is greater than 10");
+        if(a<10)
+        System.out.println("a is lesser than 10");
+
+    }
+    
+}
