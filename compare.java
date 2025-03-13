e81fc8f076cb80f3666b51866876b1c317b9dde9
sumi2901 <bahardeen784gmail.com>
2025-03-13 15:13:27 +0530

    fix

---
+++ compare.java
@@ -0,0 +1,16 @@
+public class compare {
+    public static void main(String[]args)
+    {
+        int a=9;
+        if(a==10)
+           {
+            System.out.println("a value is equal");
+        }
+            else if(a>10){
+            System.out.println("a value is greater than 10");}
+            else{
+                System.out.println("a value is smaller than 10");
+        }
+    }
+    
+}
