77a963ec5b06f3589d8752d27aa7ca7a2d12b880
sumi2901 <bahardeen784gmail.com>
2025-03-13 11:23:10 +0530

    fix

---
+++ student.java
@@ -0,0 +1,13 @@
+public class student{
+    public static void main(String[]args)
+    {
+    String name="sumi";
+    String place ="epk";
+    int mark=99;
+    char grade='A';
+    System.out.println("enter the name:"+name);
+    System.out.println("enter the place:"+place);
+    System.out.println("enter the mark:"+mark);
+    System.out.println("enter the grade:"+grade);
+    }
+}
