c4101d594d448459cf59c21b04e8b1e8208ea6e4
sumi2901 <bahardeen784gmail.com>
2025-03-13 12:07:37 +0530

    fix

---
+++ comparision.java
@@ -0,0 +1,15 @@
+public class comparision{
+    public static void main(String[]args)
+    {
+  String a="sumi";
+  String b="sumii";
+  if(a==b)
+  {
+  System.out.println("True");
+  }
+  else
+  {
+  System.out.println("False");
+  }
+}
+}
