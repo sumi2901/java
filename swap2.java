2d43024f7702fd922e09c34492cc788a14c253fe
sumi2901 <bahardeen784gmail.com>
2025-03-13 13:55:13 +0530

    fix

---
+++ swap2.java
@@ -0,0 +1,14 @@
+public class swap2{
+    public static void main (String[]args)
+    {
+        int a=5;
+        int temp;
+        int b=6;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swapping the value of a:"+a);
+        System.out.println("after swapping the value of b:"+b);
+
+    }
+}
