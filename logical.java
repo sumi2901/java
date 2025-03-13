22a291f55355ad9865542dab35aa2eb480a6406a
sumi2901 <bahardeen784gmail.com>
2025-03-13 12:09:31 +0530

    fix

---
+++ logical.java
@@ -0,0 +1,13 @@
+public class logical{
+    public static void main(String[]args)
+    {
+        int mark1=100;
+        int mark2=99;
+        char grade1='A';
+        char grade2='A';
+        if(mark1<mark2&&grade1==grade2){
+        System.out.println("True");}
+        else{
+        System.out.println("False");}
+    }
+}
