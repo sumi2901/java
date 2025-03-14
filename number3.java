20400b2db929a2d021fa6aa5a5061578596a2bf7
sumi2901 <bahardeen784gmail.com>
2025-03-14 11:53:31 +0530

    f

---
+++ number3.java
@@ -0,0 +1,20 @@
+public class number3{
+    
+        public static void main(String[]args)
+        {
+           {
+             int i=0;
+            for(i=0;i<=10;i++)
+            {
+            System.out.println("numbers are:"+i);
+        if( i==7)
+        {
+            break;
+        }
+    }
+}
+}
+}
+
+
+
