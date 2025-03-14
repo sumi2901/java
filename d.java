85bf73d28af0d7550fd9866d33e886bf2a270012
sumi2901 <bahardeen784gmail.com>
2025-03-14 11:01:18 +0530

    a

---
+++ d.java
@@ -0,0 +1,13 @@
+public class d {
+    public static void main(String[]args)
+    {
+        int i=0;
+        do
+        {
+            System.out.println("the numbers are:"+i);
+            i++;
+        }
+        while(i<=5);
+    }
+    
+}
