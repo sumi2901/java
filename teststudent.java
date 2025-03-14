79854f0db83bef754808b2e6b9051341a6912b97
sumi2901 <bahardeen784gmail.com>
2025-03-14 15:50:27 +0530

    l

---
+++ teststudent.java
@@ -0,0 +1,12 @@
+                                                                                                      
+   class student1{
+        int id;
+        String name;
+                public String String;
+                    }public class teststudent{
+                        public static void main(String[]args){
+                    student1 s1=new student1();
+                    s1.id=101;
+                    System.out.println(s1.id);
+                s1.String="sumi";
+System.out.println(s1.String);}}
