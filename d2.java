3310b591827e3fafd65174532f7fa4d0c05aede4
sumi2901 <bahardeen784gmail.com>
2025-03-14 11:02:07 +0530

    m

---
+++ d2.java
@@ -0,0 +1,12 @@
+public class d2 {
+    public static void main(String[]args)
+    {
+        int i=1;
+        do{
+            System.out.println("10");
+i++;
+        }
+        while(i<=10);
+    }
+    
+}
