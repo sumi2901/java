a46463255f02a9ab7bc6be77f4fff0ca5cbcccf1
sumi2901 <bahardeen784gmail.com>
2025-03-14 15:07:35 +0530

    l

---
+++ fruit.java
@@ -0,0 +1,9 @@
+public class fruit {
+    public static void main(String[]args)
+    {
+        String[] a={"apple","mango"};
+        for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+    }
+    
+}
