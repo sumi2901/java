074ba39bb216173e2fbf466a44fb0a64585bedcc
sumi2901 <bahardeen784gmail.com>
2025-03-14 13:22:49 +0530

    h

---
+++ wel.java
@@ -0,0 +1,12 @@
+public  class wel {
+    static void mymethod(){
+        int a=5;
+int b=6;
+ int c = a*b;
+        System.out.println("value is:"+c);
+    }
+        public static void main(String[]args){
+mymethod();
+    }
+}
+ 
