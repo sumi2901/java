37a6873bd4e7b3f69b9710b3cdd6bf530f30c213
sumi2901 <bahardeen784gmail.com>
2025-03-14 10:40:21 +0530

    fix

---
+++ number2.java
@@ -0,0 +1,12 @@
+public class number2 {
+    public static void main(String[]args)
+    {
+        int i=1;
+        while(i<=5)
+        {
+            System.out.println(1);
+            i++;
+        }
+    }
+    
+}
