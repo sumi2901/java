89533f456b9f46eb4256a58fa24575d03ae1f8a5
sumi2901 <bahardeen784gmail.com>
2025-03-14 11:33:10 +0530

    j

---
+++ f2.java
@@ -0,0 +1,13 @@
+public class f2 {
+    public static void main(String[]args)
+    {
+        int i=10;
+        for(i=10;i>5;i--)
+        {
+            System.out.println("i");
+        }
+    }
+    
+}
+
+
